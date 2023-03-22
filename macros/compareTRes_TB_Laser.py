d7fe7a8880c76de73f21a4569f4ae81d60c803e0
Martina <martina.malberti@cern.ch>
2023-03-22 16:14:16 +0100

    updates

---
+++ macros/compareTRes_TB_Laser.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+import os
+import shutil
+import glob
+import math
+import array
+import sys
+import time
+import argparse
+import json
+
+import ROOT
+import CMS_lumi, tdrstyle
+
+#set the tdr style
+tdrstyle.setTDRStyle()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(1)
+ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetLabelSize(0.055,'X')
+ROOT.gStyle.SetLabelSize(0.055,'Y')
+ROOT.gStyle.SetTitleSize(0.07,'X')
+ROOT.gStyle.SetTitleSize(0.07,'Y')
+ROOT.gStyle.SetTitleOffset(1.05,'X')
+ROOT.gStyle.SetTitleOffset(1.1,'Y')
+ROOT.gStyle.SetLegendFont(42)
+ROOT.gStyle.SetLegendTextSize(0.045)
+ROOT.gStyle.SetPadTopMargin(0.07)
+ROOT.gROOT.SetBatch(True)
+ROOT.gErrorIgnoreLevel = ROOT.kWarning
+
+
+
+
+fnames = { (25, 'TB') : '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/plots/summaryPlots_HPK_nonIrr_LYSO813.root',
+           (20, 'TB') : '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/plots/summaryPlots_HPK_nonIrr_LYSO814.root',
+           (15, 'TB') : '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/plots/summaryPlots_HPK_nonIrr_LYSO528.root',
+
+           (25, 'Laser') : '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/data/plots_LYSO743_SiPM_HPK_C25-3-T2_3.root',
+           (20, 'Laser'): '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/data/plots_LYSO743_SiPM_HPK_C20-3-T2_2.root',
+           (15, 'Laser') : '/afs/cern.ch/work/m/malberti/MTD/TBatFNALMar2023/Lab5015Analysis/data/plots_LYSO743_SiPM_HPK_2-32.root',
+}
+
+
+gnames = { (25, 'TB') : 'g_deltaT_totRatioCorr_bestTh_vs_vov_enBin01_average',
+           (20, 'TB') : 'g_deltaT_totRatioCorr_bestTh_vs_vov_enBin01_average',
+           (15, 'TB') : 'g_deltaT_totRatioCorr_bestTh_vs_vov_enBin01_average',
+
+           (25, 'Laser') : 'g_tRes_vs_Vov_LYSO743_SiPM_HPK_C25-3-T2_3_tune19',
+           (20, 'Laser') : 'g_tRes_vs_Vov_LYSO743_SiPM_HPK_C20-3-T2_2_tune72',
+           (15, 'Laser') : 'g_tRes_vs_Vov_LYSO743_SiPM_HPK_2-32_tune78',
+}
+
+plotAttrs = { (25, 'TB') : [20, ROOT.kBlack,  'TB - 25#mum SiPMs'],
+              (20, 'TB') : [20, ROOT.kRed,  'TB - 20#mum SiPMs' ],
+              (15, 'TB') : [20, ROOT.kGreen,'TB - 15#mum SiPMs'],
+              (25, 'Laser') : [24, ROOT.kBlack, 'Laser - 25#mum SiPMs'],
+              (20, 'Laser') : [24, ROOT.kRed+2,   'Laser - 20#mum SiPMs'],
+              (15, 'Laser') : [24, ROOT.kGreen+2,  'Laser - 15#mum SiPMs'],
+}
+
+
+c = ROOT.TCanvas('c_comparison_TB_Laser_cellSizes','c_comparison_TB_Laser_cellSizes')
+hPad = ROOT.gPad.DrawFrame(0.,0.,4.,175.)
+hPad.SetTitle(";V_{OV};#sigma_{t}^{bar} [ps]")
+hPad.Draw()
+ROOT.gPad.SetGridx()
+ROOT.gPad.SetGridy()
+
+leg = ROOT.TLegend(0.60, 0.60, 0.89, 0.89)
+leg.SetBorderSize(0)
+leg.SetFillStyle(0)                                                                                                                                                                                    
+leg.SetTextFont(42)                                                                                                                                                                                    
+leg.SetTextSize(0.04) 
+
+g = {}
+f = {}
+
+for data in ['Laser', 'TB']:
+    for cell in [25,20,15]:
+        f[(cell,data)] = ROOT.TFile.Open(fnames[(cell,data)])
+        g[(cell,data)] = f[(cell,data)].Get(gnames[(cell,data)])
+        g[(cell,data)].SetMarkerSize(1)
+        g[(cell,data)].SetMarkerStyle(plotAttrs[(cell,data)][0])
+        g[(cell,data)].SetMarkerColor(plotAttrs[(cell,data)][1])
+        g[(cell,data)].SetLineColor(plotAttrs[(cell,data)][1])
+        leg.AddEntry(g[(cell,data)], '%s'%plotAttrs[(cell,data)][2],'PL')
+        if (data == 'Laser'): 
+            g[(cell,data)].Draw('plsame')
+        else: 
+            g[(cell,data)].Draw('psame')
+leg.Draw()
+
+c.SaveAs('/eos/user/m/malberti/www/MTD/TOFHIR2X/MTDTB_FNAL_Mar23/ModuleCharacterization/%s.png'%c.GetName())
+c.SaveAs('/eos/user/m/malberti/www/MTD/TOFHIR2X/MTDTB_FNAL_Mar23/ModuleCharacterization/%s.pdf'%c.GetName())
+
+
+for vov in [1.0, 1.5, 3.5]:
+    for cell in [25,20,15]:
+        res1 = g[(cell,'TB')].Eval(vov)
+        res2 = g[(cell,'Laser')].Eval(vov)
+        if (res1>res2):
+            print vov, cell, res1, res2, math.sqrt(res1*res1-res2*res2)
---
+++ macros/makeAllSummaryPlots.sh
@@ -0,0 +1,8 @@
+# LYSO8528 (15um)
+python moduleCharacterizationSummaryPlots.py -m 2 -i HPK_nonIrr_LYSO528_Vov1.50,HPK_nonIrr_LYSO528_Vov2.00,HPK_nonIrr_LYSO528_Vov3.50 -o HPK_nonIrr_LYSO528
+
+# LYSO814 (20um)
+python moduleCharacterizationSummaryPlots.py -m 2 -i HPK_nonIrr_LYSO814_Vov0.50,HPK_nonIrr_LYSO814_Vov0.80,HPK_nonIrr_LYSO814_Vov1.00,HPK_nonIrr_LYSO814_Vov1.50,HPK_nonIrr_LYSO814_Vov3.50 -o HPK_nonIrr_LYSO814
+
+# LYSO813 (25um)
+python moduleCharacterizationSummaryPlots.py -m 2 -i HPK_nonIrr_LYSO813_Vov0.50,HPK_nonIrr_LYSO813_Vov0.80,HPK_nonIrr_LYSO813_Vov1.00,HPK_nonIrr_LYSO813_Vov1.50,HPK_nonIrr_LYSO813_Vov2.00,HPK_nonIrr_LYSO813_Vov3.50 -o HPK_nonIrr_LYSO813
