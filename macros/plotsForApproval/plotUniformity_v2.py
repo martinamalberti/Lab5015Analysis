c872940c4cc6bdd48dd4cc7a86431fc1dea69799
Martina <martina.malberti@cern.ch>
2023-12-31 18:10:02 +0100

    updates

---
+++ macros/plotsForApproval/plotUniformity_v2.py
@@ -0,0 +1,154 @@
+#! /usr/bin/env python
+import os
+import shutil
+import glob
+import math
+import array
+import sys
+import time
+import argparse
+import json
+
+import ROOT
+import CMS_lumi, tdrstyle
+
+from utils import *
+
+#set the tdr style
+tdrstyle.setTDRStyle()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(0)
+ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetLabelSize(0.055,'X')
+ROOT.gStyle.SetLabelSize(0.055,'Y')
+ROOT.gStyle.SetTitleSize(0.07,'X')
+ROOT.gStyle.SetTitleSize(0.07,'Y')
+ROOT.gStyle.SetTitleOffset(1.05,'X')
+ROOT.gStyle.SetTitleOffset(1.1,'Y')
+ROOT.gStyle.SetLegendFont(42)
+ROOT.gStyle.SetLegendTextSize(0.045)
+ROOT.gStyle.SetPadTopMargin(0.07)
+ROOT.gROOT.SetBatch(True)
+ROOT.gErrorIgnoreLevel = ROOT.kWarning
+
+from VovsEff import *
+# Import file with VovEff and DCR
+with open('/eos/cms/store/group/dpg_mtd/comm_mtd/TB/MTDTB_H8_May2023/VovsEff_v2.json', 'r') as f:
+   data = json.load(f)
+
+
+inputdir = '/eos/cms/store/group/dpg_mtd/comm_mtd/TB/MTDTB_H8_May2023/ANALYSIS/'
+outdir   = '/eos/user/m/malberti/www/MTD/plotsForConferences2023/v2/'
+
+
+#modules = {'HPK_nonIrr_LYSO813_T5C', 'HPK_2E14_LYSO815_T-35C', 'HPK_2E14_LYSO825_T-35C', 'HPK_1E14_LYSO819_T-22C'}
+modules = {'HPK_nonIrr_LYSO813_T5C', 'HPK_2E14_LYSO815_T-35C'}
+
+fnames = { 'HPK_nonIrr_LYSO813_T5C' : inputdir+'TOFHIR2C/ModuleCharacterization/summaryPlots_HPK_nonIrr_LYSO813_T5C.root',
+           'HPK_2E14_LYSO815_T-35C' : inputdir+'TOFHIR2C/ModuleCharacterization/summaryPlots_HPK_2E14_LYSO815_T-35C.root',
+           'HPK_2E14_LYSO825_T-35C' : inputdir+'TOFHIR2X/ModuleCharacterization/summaryPlots_HPK_2E14_LYSO825_T-35C.root',
+           'HPK_1E14_LYSO819_T-22C' : inputdir+'TOFHIR2X/ModuleCharacterization/summaryPlots_HPK_1E14_LYSO819_T-22C.root',
+       }
+
+labels = {'HPK_nonIrr_LYSO813_T5C' : '#splitline{Type 2}{25 #mum, non irradiated}',
+          'HPK_2E14_LYSO815_T-35C' : '#splitline{Type 2}{25 #mum, irradiated}',
+          'HPK_2E14_LYSO825_T-35C' : 'HPK (20 #mum, 2E14), type 2',
+          'HPK_1E14_LYSO819_T-22C' : 'HPK (25 #mum, 1E14), type 1',
+     }
+
+
+Vovs = { 'HPK_nonIrr_LYSO813_T5C' : [0.80, 1.25, 1.50, 3.50],
+         'HPK_2E14_LYSO815_T-35C' : [1.00, 1.25, 1.50, 2.00],
+         'HPK_2E14_LYSO825_T-35C' : [0.80, 1.00, 1.25, 1.50, 2.00],
+         'HPK_1E14_LYSO819_T-22C' : [0.80, 1.00, 1.25, 1.50, 2.00],
+}
+
+bestVovs = { 'HPK_nonIrr_LYSO813_T5C' : 0.80,
+             #'HPK_2E14_LYSO815_T-35C' : 1.50,
+             'HPK_2E14_LYSO815_T-35C' : 1.00,
+             'HPK_2E14_LYSO825_T-35C' : 1.50,
+             'HPK_1E14_LYSO819_T-22C' : 2.00
+
+          }
+
+h = {}
+h_all = ROOT.TH1F('h_all','h_all', 40, -0.8,0.8)
+h_irr = ROOT.TH1F('h_irr','h_irr', 40, -0.8,0.8)
+
+for mod in modules:
+   f = ROOT.TFile.Open(fnames[mod])
+   
+   c = ROOT.TCanvas('c_timeResolution_vs_bar_%s'%mod,'c_timeResolution_vs_bar_%s'%mod, 600, 500)
+   hPad = ROOT.TH2F('hPad','', 100, -0.5, 15.5,100, 0,130)
+   hPad.SetTitle("; bar; time resolution [ps]")
+   hPad.Draw()
+   c.SetGridy()
+   c.SetTicks()
+
+   leg = ROOT.TLegend(0.20, 0.68, 0.50, 0.88)
+   leg.SetBorderSize(0)
+   leg.SetFillStyle(0)
+   if (len(Vovs[mod])>4):
+      leg.SetNColumns(2);
+      leg.SetColumnSeparation(0.2);
+
+   h[mod] = ROOT. TH1F('h_%s'%mod,'h_%s'%mod, 50, -0.75,0.75 )
+   h[mod].SetLineColor(1)
+   h[mod].SetLineWidth(1)
+
+   for iv,vov in enumerate(Vovs[mod]):
+      
+      print vov, mod
+      g = f.Get('g_deltaT_totRatioCorr_bestTh_vs_bar_Vov%.2f_enBin01'%vov) 
+      print('g_deltaT_totRatioCorr_bestTh_vs_bar_Vov%.2f_enBin01'%vov)
+      print(g.GetN())
+
+      for i in range(0,g.GetN()):
+         x = (g.GetPointY(i) - g.GetMean(2) )/g.GetMean(2)
+         h[mod].Fill(x)
+         
+      g.SetMarkerStyle(20+iv)
+      g.SetMarkerSize(1)
+      if ( mod == 'HPK_2E14_LYSO815_T-35C' and vov == 1.00):
+         g.SetMarkerColor(ROOT.kAzure+10)
+         g.SetLineColor(ROOT.kAzure+10)
+      g.Draw('psame')
+         
+      ovEff = vov
+      if ('2E14' in mod or '1E14' in mod or '1E13' in mod):
+         ovEff = getVovEffDCR(data, mod, ('%.02f'%vov))[0]
+      leg.AddEntry(g, 'V_{OV} = %.2f V'%ovEff, 'PL') 
+   leg.Draw()
+
+
+   hPad2 = ROOT.TPad("subpad","",0.6, 0.6, 0.92, 0.92)
+   hPad2.SetBottomMargin(0.2)
+   hPad2.Draw()
+   hPad2.cd()
+   h[mod].Draw()
+   h[mod].GetXaxis().SetNdivisions(5)
+   h[mod].GetYaxis().SetNdivisions(5)
+   h[mod].GetXaxis().SetLabelSize(0.090)
+   h[mod].GetYaxis().SetLabelSize(0.090)
+   h[mod].GetXaxis().SetTitleSize(0.090)
+   h[mod].GetXaxis().SetTitle('(#sigma_{t} - <#sigma_{t}>)/<#sigma_{t}>')
+   latex2 = ROOT.TLatex(0.65,0.75,'RMS = %.0f %%'%(h[mod].GetRMS()*100))
+   latex2.SetNDC()
+   latex2.SetTextSize(0.080)
+   latex2.SetTextFont(42)
+   latex2.Draw()
+   
+   
+   c.cd()
+   latex = ROOT.TLatex(0.20,0.22,'%s'%(labels[mod]))
+   latex.SetNDC()
+   latex.SetTextSize(0.050)
+   latex.SetTextFont(42)
+   latex.Draw()
+
+   cms_logo = draw_logo()
+   cms_logo.Draw()
+
+   c.SaveAs(outdir+'%s.png'%c.GetName())
+   c.SaveAs(outdir+'%s.pdf'%c.GetName())
+   hPad.Delete()
---
+++ macros/test_2Xto2C_scaling.py
@@ -0,0 +1,136 @@
+#! /usr/bin/env python
+import os
+import shutil
+import glob
+import math
+import array
+import sys
+import time
+import argparse
+import json
+
+import ROOT
+import CMS_lumi, tdrstyle
+from utils import *
+from SiPM import *
+
+#set the tdr style
+tdrstyle.setTDRStyle()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(1)
+ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetLabelSize(0.055,'X')
+ROOT.gStyle.SetLabelSize(0.055,'Y')
+ROOT.gStyle.SetTitleSize(0.07,'X')
+ROOT.gStyle.SetTitleSize(0.07,'Y')
+ROOT.gStyle.SetTitleOffset(1.05,'X')
+ROOT.gStyle.SetTitleOffset(1.1,'Y')
+ROOT.gStyle.SetLegendFont(42)
+ROOT.gStyle.SetLegendTextSize(0.045)
+ROOT.gStyle.SetPadTopMargin(0.07)
+ROOT.gROOT.SetBatch(True)
+ROOT.gErrorIgnoreLevel = ROOT.kWarning
+
+
+labels = ['HPK_2E14_LYSO815_T-35C', 'HPK_2E14_LYSO825_T-35C']
+
+outdir = '/eos/user/m/malberti/www/MTD/TOFHIR2C/MTDTB_CERN_May23/Test_2Xto2C/'
+
+fnames = { '2X' : '/eos/user/m/malberti/www/MTD/TOFHIR2X/MTDTB_CERN_May23/timeResolution_2E14_20um_25um_T2_v2/plots_timeResolution_2E14_20um_25um_T2_TBMay23_TOFHIR2X.root',
+           '2C' : '/eos/user/m/malberti/www/MTD/TOFHIR2C/MTDTB_CERN_May23/timeResolution_2E14_20um_25um_T2/plots_timeResolution_2E14_20um_25um_T2_TBMay23_TOFHIR2C.root'
+}
+
+plotAttrs = { '2X' : [20, ROOT.kRed,  'TOFHIR2X'],
+              '2C' : [21, ROOT.kGreen+2,'TOFHIR2C'],
+}
+
+
+gNoise = {}
+gStoch = {}
+gDCR = {}
+gSR = {}
+gData = {}
+gData_scaled = {}
+f = {}
+
+for tofhir in ['2X','2C']:
+    f[tofhir] = ROOT.TFile.Open(fnames[tofhir])
+    suffix = ''
+    if (tofhir == '2C'): suffix = '_TOFHIR2C'
+    for label in labels:
+        gNoise[tofhir, label] = f[tofhir].Get('g_Noise_vs_Vov_average_%s'%(label+suffix))
+        gStoch[tofhir, label] = f[tofhir].Get('g_Stoch_vs_Vov_average_%s'%(label+suffix))
+        gDCR[tofhir, label]   = f[tofhir].Get('g_DCR_vs_Vov_average_%s'%(label+suffix))
+        gSR[tofhir, label]    = f[tofhir].Get('g_SR_vs_Vov_average_%s'%(label+suffix))
+        gData[tofhir, label] = f[tofhir].Get('g_data_vs_Vov_average_%s'%(label+suffix))
+        gData_scaled[tofhir, label] = ROOT.TGraphErrors()
+    
+        # scale noise 2X --> 2C
+        if (tofhir == '2X'):
+            for i in range(0, gData[tofhir, label].GetN()):
+                vov = gData[tofhir, label].GetX()[i]
+                sr = gSR[tofhir, label].Eval(vov)
+                s_noise_scaled = sigma_noise(sr*1.20, '2C')
+                s_stoch = gStoch[tofhir, label].Eval(vov)
+                s_dcr = gDCR[tofhir, label].Eval(vov)
+                s_tot = math.sqrt(s_noise_scaled*s_noise_scaled + s_stoch*s_stoch + s_dcr*s_dcr)
+                s_tot_up = math.sqrt(s_noise_scaled*s_noise_scaled*1.05*1.05 + s_stoch*s_stoch + s_dcr*s_dcr) # assume 5% error on noise estimation (... to be checked)
+                s_tot_down = math.sqrt(s_noise_scaled*s_noise_scaled*0.95*0.95 + s_stoch*s_stoch + s_dcr*s_dcr)
+                gData_scaled[tofhir, label].SetPoint(i, vov, s_tot)
+                #gData_scaled[tofhir, label].SetPointError(i, 0, gData[tofhir, label].GetErrorY(i))
+                gData_scaled[tofhir, label].SetPointError(i, 0, abs(s_tot_up-s_tot_down)/2)
+        
+                
+# plot
+for label in labels:
+    c = ROOT.TCanvas('c_timeResolution_%s_scaling2Xto2C'%label,'c_timeResolution_%s_scaling2Xto2C'%label, 600, 500)
+    leg = ROOT.TLegend(0.50, 0.65, 0.89, 0.89)
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.SetTextFont(42)
+    leg.SetTextSize(0.045)
+    hPad = ROOT.gPad.DrawFrame(0.,40.,1.8,110.)
+    hPad.SetTitle(";V_{OV} [V];time resolution [ps]")
+    hPad.Draw()
+    ROOT.gPad.SetTicks(1)
+    for tofhir in ['2X','2C']:
+        suffix = ''
+        if (tofhir == '2C'): suffix = '_TOFHIR2C'
+        gData[tofhir,label].SetMarkerSize(1)
+        gData[tofhir,label].SetMarkerStyle(plotAttrs[tofhir][0])
+        gData[tofhir,label].SetMarkerColor(plotAttrs[tofhir][1])
+        gData[tofhir,label].SetLineColor(plotAttrs[tofhir][1])
+        gData[tofhir,label].Draw('plsame')
+        leg.AddEntry(gData[tofhir,label], '%s'%plotAttrs[tofhir][2],'PL')
+        if (tofhir == '2X'):
+            gData_scaled[tofhir,label].SetLineColor(plotAttrs[tofhir][1]+2)
+            gData_scaled[tofhir,label].SetLineStyle(2)
+            gData_scaled[tofhir,label].SetLineWidth(2)
+            gData_scaled[tofhir,label].SetFillStyle(1)
+            gData_scaled[tofhir,label].SetFillColorAlpha(plotAttrs[tofhir][1]+2, 0.2)
+            
+            leg.AddEntry(gData_scaled[tofhir,label], '%s - scaled'%plotAttrs[tofhir][2],'FL')
+            gData_scaled[tofhir,label].Draw('E3lsame')
+    gData_scaled['2X',label].Draw('lsame')
+    leg.Draw()
+
+    tl2 = ROOT.TLatex()
+    tl2.SetNDC()
+    tl2.SetTextFont(42)
+    tl2.SetTextSize(0.045)
+
+    tl = ROOT.TLatex()
+    tl.SetNDC()
+    tl.SetTextFont(42)
+    tl.SetTextSize(0.045)
+    tl.DrawLatex(0.58,0.20,'2 #times 10^{14} 1 MeV n_{eq}/cm^{2}')
+
+    cms_logo = draw_logo()
+    cms_logo.Draw()
+
+    c.SaveAs(outdir+'%s.png'%c.GetName())
+    c.SaveAs(outdir+'%s.pdf'%c.GetName())
+
+
+
+
