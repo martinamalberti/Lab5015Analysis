627d9d6084cd3b1092dd7e06f589e468a7bb3474
Martina <martina.malberti@cern.ch>
2024-01-07 21:53:38 +0100

    updates

--- scripts/drawPulseShapeCondor.sh
+++ scripts/drawPulseShapeCondor.sh
@@ -3,9 +3,11 @@
 echo
 echo 'START---------------'
 echo 'current dir: ' ${PWD}
-cd /afs/cern.ch/work/m/malberti/MTD/TBatH8May2023/Lab5015Analysis/
+cd /afs/cern.ch/work/m/malberti/MTD/TBatH8Sep2023/Lab5015Analysis/
 echo 'current dir: ' ${PWD}
 source scripts/setup.sh
+echo $LD_LIBRARY_PATH
+echo $ROOT_INCLUDE_PATH
 ./bin/drawPulseShapeTB.exe $1
 echo 'STOP---------------'
 echo
